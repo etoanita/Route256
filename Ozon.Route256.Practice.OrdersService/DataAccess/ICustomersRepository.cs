a9244552b87e89e35f63cf37ad5c824147afad07
Volkova Anna <anna.mikhaylova09@gmail.com>
2024-03-31 21:37:08 +0300

    [CSHW4]: use cache for customers

---
+++ Ozon.Route256.Practice.OrdersService/DataAccess/ICustomersRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Ozon.Route256.Practice.OrdersService.DataAccess
+{
+    public interface ICustomersRepository
+    {
+        Task<Customer?> Find(long customerId, CancellationToken token);
+        Task Insert(Customer customerId, CancellationToken token);
+
+    }
+}
--- Ozon.Route256.Practice.OrdersService/Handlers/OrderRegistration/OrderRegistrationHandler.cs
+++ Ozon.Route256.Practice.OrdersService/Handlers/OrderRegistration/OrderRegistrationHandler.cs
@@ -10,15 +10,18 @@ public class OrderRegistrationHandler : IOrderRegistrationHandler
 {
     private readonly IOrdersRepository _orderRepository;
     private readonly IRegionsRepository _regionsRepository;
+    private readonly ICustomersRepository _customersRepository;
     private readonly CustomersClient _customersClient;
     private readonly IOrderProducer _producer;
     private readonly ILogger _logger;
 
     public OrderRegistrationHandler(IOrdersRepository orderRepository, IRegionsRepository regionsRepository, 
-        CustomersClient customersClient, IOrderProducer producer, ILogger<OrderRegistrationHandler> logger)
+        CustomersClient customersClient, IOrderProducer producer, ICustomersRepository customersRepository,
+        ILogger<OrderRegistrationHandler> logger)
     {
         _orderRepository = orderRepository;
         _regionsRepository = regionsRepository;
+        _customersRepository = customersRepository;
         _customersClient = customersClient;
         _producer = producer; 
         _logger = logger;
@@ -31,26 +34,30 @@ public class OrderRegistrationHandler : IOrderRegistrationHandler
         if (orderAlreadyRegistered)
             throw new Exception($"Order {order.Id} already registered");
 
-        GetCustomerByIdResponse result;
+        Customer customer;
         try
         {
-            result = await _customersClient.GetCustomerByIdAsync(new GetCustomerByIdRequest { Id = order.Customer.Id }, cancellationToken: token);
+            customer = await _customersRepository.Find(order.Customer.Id, token);
+            if (customer == null)
+            {
+                var result = await _customersClient.GetCustomerByIdAsync(new GetCustomerByIdRequest { Id = order.Customer.Id }, cancellationToken: token);
+                customer = result.Customer;
+            }
+            else
+                _logger.LogInformation("Get customer data from redis cache");
         }
         catch (Exception ex)
         {
-            //log
-            result = new GetCustomerByIdResponse
+            _logger.LogError(ex.Message); //TODO: handle correctly
+            customer = new Customer
             {
-                Customer = new Customer
-                {
-                    Id = order.Customer.Id,
-                    FirstName = "TestCustFirstName",
-                    LastName = "TestCustLastName",
-                    MobileNumber = "12344567890"
-                }
+                Id = order.Customer.Id,
+                FirstName = "TestCustFirstName",
+                LastName = "TestCustLastName",
+                MobileNumber = "12344567890"
             };
         }
-        var orderEntity = Converters.CreateOrderEntity(order, result.Customer, DateTime.UtcNow);
+        var orderEntity = Converters.CreateOrderEntity(order, customer, DateTime.UtcNow);
         await _orderRepository.InsertAsync(orderEntity, token);
 
         var custAddress = order.Customer.Address;
